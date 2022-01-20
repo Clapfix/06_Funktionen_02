19a318a3080621c5a2e1b6d0ae749f38c4eb51df
Clapfix <ronny.perner@kabelmail.de>
2022-01-20 11:18:37 +0100

    modul division a / b

--- script_01.js
+++ script_01.js
@@ -9,19 +9,31 @@
 */
 
 // modul division a / b | test:
+// output(divide(9,3));
+// output(divide(120,10));
+// output(divide(10,2));
+// output(divide(5,5));
+output(divide(0,2));
+output(divide(2,0));              
 
+function divide(a,b){
+    
+    if (b==0) {
+        return("Division durch 0 ist nicht möglich!");
+    } else {
+        return a/b;
+    }
+}
 
 // modul multiplikation a * b | test:
-output(multiply(6,3));
-output(multiply(3,3));
-output(multiply(-3,3));
-output(multiply(3,0));
+// output(multiply(6,3));
+// output(multiply(3,3));
+// output(multiply(-3,3));
+//output(multiply(3,0));
 function multiply(a,b) {
     return a * b;
     
 }
-
-
 // modul subtraction a - b | test:
 // output(subtraction(5,3));
 // output(subtraction(3,5));
@@ -29,17 +41,13 @@ function subtraction(a,b) {
     return a - b;
     
 }
-
-
 // modul: addition a + b | test:
 // output(add(2,3));
 // output(add(2,-2));
 // output(add(2.4,2.1));          
-
 function add(a,b) {
         return a + b;
 }
-
 function output(outputData) {               
     console.log(outputData);
 }
